5571611a5fbed9c09571ae6b2b9b4c51d7dddd64
unknown <rprudencio@ALGDES004.ALIGNET.com>
2015-08-11 15:56:15 -0500

    ac

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
              $("#one").backstretch([
                   "image/7.jpg"
                 , "image/1.jpg"
-                ,"image/6.jpg"
+               
 
                 ], {fade: 800,
         duration: 5000}
